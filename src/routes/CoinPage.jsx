4ba30d017b61852ffe68ff2d8f1802d6a751cc44
Nitin Sharma <nitins9868@gmail.com>
2023-01-18 23:50:13 +0530

    some minor features

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -10,7 +10,6 @@ const CoinPage = () => {
   const params = useParams();
   // console.log(params);
   const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}?tickers=true&market_data=true&sparkline=true`;
-
   useEffect(() => {
     axios.get(url).then((response) => {
       setCoin(response.data);
@@ -18,16 +17,19 @@ const CoinPage = () => {
     });
   }, [url]);
 
+  // console.log(coin?.links?.homepage[0]);
+
   return (
     <div className="rounded-div my-12 py-8">
-      <div className="flex py-8">
-        <img className="w-20 mr-8" src={coin?.image?.large} alt="/" />
-        <div>
-          <p className="text-3xl font-bold">{coin?.name} price</p>
-          <p>({coin?.symbol?.toUpperCase()} / USD)</p>
+      <a href={`${coin?.links?.homepage[0]}`} target="_blank" rel="noreferrer">
+        <div className="flex py-8">
+          <img className="w-20 mr-8" src={coin?.image?.large} alt="/" />
+          <div>
+            <p className="text-3xl font-bold">{coin?.name} price</p>
+            <p>({coin?.symbol?.toUpperCase()} / USD)</p>
+          </div>
         </div>
-      </div>
-
+      </a>
       <div className="grid md:grid-cols-2 gap-8">
         <div>
           <div className="flex justify-between">
